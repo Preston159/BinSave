e04da04537a3bb138ce54f7fb229c8e1c797956b
Preston Petrie <me@preston159.com>
2018-06-10 01:08:50 -0500

    Added new integer data types

--- BinSave/src/com/preston159/binsave/DataType.java
+++ BinSave/src/com/preston159/binsave/DataType.java
@@ -42,6 +42,26 @@ public enum DataType {
 	 */
 	INT_32BIT(4),
 	/**
+	 * <p><strong>Length:</strong> 5 bytes</p>
+	 * <p><strong>Stores:</strong> a 40-bit signed integer</p>
+	 */
+	INT_40BIT(5),
+	/**
+	 * <p><strong>Length:</strong> 6 bytes</p>
+	 * <p><strong>Stores:</strong> a 48-bit signed integer</p>
+	 */
+	INT_48BIT(6),
+	/**
+	 * <p><strong>Length:</strong> 7 bytes</p>
+	 * <p><strong>Stores:</strong> a 56-bit signed integer</p>
+	 */
+	INT_56BIT(7),
+	/**
+	 * <p><strong>Length:</strong> 8 bytes</p>
+	 * <p><strong>Stores:</strong> a 64-bit signed integer</p>
+	 */
+	INT_64BIT(8),
+	/**
 	 * <p><strong>Length:</strong> 1 byte</p>
 	 * <p><strong>Stores:</strong> an 8-bit unsigned integer</p>
 	 */
@@ -56,6 +76,26 @@ public enum DataType {
 	 * <p><strong>Stores:</strong> a 24-bit unsigned integer</p>
 	 */
 	UINT_24BIT(3),
+	/**
+	 * <p><strong>Length:</strong> 4 bytes</p>
+	 * <p><strong>Stores:</strong> a 32-bit unsigned integer</p>
+	 */
+	UINT_32BIT(4),
+	/**
+	 * <p><strong>Length:</strong> 5 bytes</p>
+	 * <p><strong>Stores:</strong> a 40-bit unsigned integer</p>
+	 */
+	UINT_40BIT(5),
+	/**
+	 * <p><strong>Length:</strong> 6 bytes</p>
+	 * <p><strong>Stores:</strong> a 48-bit unsigned integer</p>
+	 */
+	UINT_48BIT(6),
+	/**
+	 * <p><strong>Length:</strong> 7 bytes</p>
+	 * <p><strong>Stores:</strong> a 56-bit unsigned integer</p>
+	 */
+	UINT_56BIT(7),
 	/**
 	 * <p><strong>Length:</strong> 1 byte</p>
 	 * <p><strong>Stores:</strong> an ASCII character</p>
